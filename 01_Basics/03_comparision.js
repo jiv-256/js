f9a7bad10dccb217760f566e85384796a556b621
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-21 00:16:08 +0530

    Add files via upload

---
+++ 01_Basics/03_comparision.js
@@ -0,0 +1,8 @@
+// console.log(2==1);
+// console.log(2 != 1);
+
+console.log("2">1);
+console.log("02" > 1);
+
+console.log("2"===2);
+//this is called strictly equal to, it check the value as well as the datatype also
