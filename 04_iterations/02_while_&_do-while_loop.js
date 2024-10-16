ca17fe19fa11bfb583f1b06b6bf5a5a3be20a504
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-10-16 23:17:53 +0530

    Add files via upload

---
+++ 04_iterations/02_while_&_do-while_loop.js
@@ -0,0 +1,33 @@
+// while loop
+let i =0 
+while (i <=10) {
+    // console.log(`value of index is ${i}`);
+    i = i+2
+}
+// output=>value of index is 0
+// value of index is 2
+// value of index is 4
+// value of index is 6
+// value of index is 8
+// value of index is 10
+let myarr = ['flash','batman','superman']
+
+let j=0
+while (j < myarr.length) {
+    console.log(`value at index ${j} is ${myarr[j]}`);
+    j++
+}
+// output =>
+// value at index 0 is flash
+// value at index 1 is batman
+// value at index 2 is superman
+
+// do while
+
+let score = 11
+do{
+    console.log(`score is ${score}`);
+    score ++
+}while (score <= 10);
+// output=>score is 11
+
